9828045ce3ac3693417dc817722bcfa891377d59
Hidetake Iwata <int128@gmail.com>
2022-09-22 17:16:36 +0900

    Initial commit

---
+++ src/generated/graphql.ts
@@ -0,0 +1,10 @@
+import * as Types from './graphql-types';
+
+export type DeploymentsAtCommitQueryVariables = Types.Exact<{
+  owner: Types.Scalars['String'];
+  name: Types.Scalars['String'];
+  expression: Types.Scalars['String'];
+}>;
+
+
+export type DeploymentsAtCommitQuery = { __typename?: 'Query', rateLimit?: { __typename?: 'RateLimit', cost: number } | null, repository?: { __typename?: 'Repository', object?: { __typename: 'Blob' } | { __typename: 'Commit', deployments?: { __typename?: 'DeploymentConnection', nodes?: Array<{ __typename?: 'Deployment', environment?: string | null, state?: Types.DeploymentState | null, latestStatus?: { __typename?: 'DeploymentStatus', description?: string | null, logUrl?: any | null, environmentUrl?: any | null } | null } | null> | null } | null } | { __typename: 'Tag' } | { __typename: 'Tree' } | null } | null };
--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,54 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { DeploymentsAtCommitQuery } from './generated/graphql'
+import { DeploymentState } from './generated/graphql-types'
+import { getDeploymentsAtCommit } from './queries/deployments'
 
 type Inputs = {
-  name: string
+  owner: string
+  repo: string
+  sha: string
+  token: string
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+type Outputs = {
+  completed: boolean
+}
+
+export const run = async (inputs: Inputs): Promise<Outputs> => {
+  const octokit = github.getOctokit(inputs.token)
+
+  core.info(`Get deployments at ${inputs.owner}/${inputs.repo}@${inputs.sha}`)
+  const deployments = await getDeploymentsAtCommit(octokit, {
+    owner: inputs.owner,
+    name: inputs.repo,
+    expression: inputs.sha,
+  })
+  core.startGroup('getDeploymentsAtCommit')
+  core.info(JSON.stringify(deployments, undefined, 2))
+  core.endGroup()
+
+  return aggregate(deployments)
+}
+
+export const aggregate = (q: DeploymentsAtCommitQuery): Outputs => {
+  if (q.repository == null) {
+    throw new Error(`q.repository === ${String(q.repository)}`)
+  }
+  if (q.repository.object?.__typename !== 'Commit') {
+    throw new Error(`q.repository.object.__typename === ${String(q.repository.object?.__typename)}`)
+  }
+
+  let completed = true
+  for (const node of q.repository.object.deployments?.nodes ?? []) {
+    if (node == null || node.environment == null || node.state == null || node.latestStatus == null) {
+      continue
+    }
+
+    if (!(node.state === DeploymentState.Active)) {
+      completed = false
+    }
+  }
+
+  return { completed }
 }
