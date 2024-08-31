c273ef1e8edccfeca1620237eede83adbfde3ce4
Hidetake Iwata <int128@gmail.com>
2024-08-31 17:22:01 +0900

    Release v1 (#628)
    * Release v1
    
    * Fix description
    
    * Refactor

--- src/run.ts
+++ src/run.ts
@@ -1,16 +1,14 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { aggregate } from './aggregate.js'
-import { listDeployments } from './queries/listDeployments.js'
 import { waitForDeployments } from './wait.js'
 
 type Inputs = {
-  waitUntil: 'completed' | 'succeeded' | undefined
-  waitInitialDelaySeconds: number
-  waitPeriodSeconds: number
+  until: 'completed' | 'succeeded'
+  initialDelaySeconds: number
+  periodSeconds: number
   owner: string
   repo: string
-  sha: string
+  deploymentSha: string
   token: string
 }
 
@@ -25,20 +23,11 @@ type Outputs = {
 export const run = async (inputs: Inputs): Promise<Outputs> => {
   const octokit = github.getOctokit(inputs.token)
 
-  if (inputs.waitUntil) {
-    core.info(`Waiting for deployments until the status is ${inputs.waitUntil}`)
-    const outputs = await waitForDeployments(octokit, inputs)
-    if (inputs.waitUntil === 'succeeded' && outputs.failed) {
-      core.setFailed(`deployment failed:\n${outputs.summary}`)
-      return outputs
-    }
+  core.info(`Waiting for deployments until the status is ${inputs.until}`)
+  const outputs = await waitForDeployments(octokit, inputs)
+  if (inputs.until === 'succeeded' && outputs.failed) {
+    core.setFailed(`Some deployment failed:\n${outputs.summary}`)
     return outputs
   }
-
-  const deployments = await listDeployments(octokit, {
-    owner: inputs.owner,
-    name: inputs.repo,
-    expression: inputs.sha,
-  })
-  return aggregate(deployments)
+  return outputs
 }
