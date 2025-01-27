c19f88b2ffe5d08ae1d6e539e32b51bd30fb1d11
Hidetake Iwata <int128@gmail.com>
2025-01-27 17:09:32 +0900

    Change workflow run summary

--- src/run.ts
+++ src/run.ts
@@ -26,18 +26,14 @@ export const run = async (inputs: Inputs): Promise<Outputs> => {
   const octokit = github.getOctokit(inputs.token)
   core.info(`Waiting for deployments until the status is ${inputs.until}`)
   const outputs = await waitForDeployments(octokit, inputs)
-  core.info(`----`)
-  core.info(`succeeded: ${outputs.succeeded}`)
-  core.info(`failed: ${outputs.failed}`)
-  core.info(`completed: ${outputs.completed}`)
-  core.info(`progressing: ${outputs.progressing}`)
-  core.info(`----`)
-  core.info(outputs.summary)
-  core.info(`----`)
   if (inputs.until === 'succeeded' && outputs.failed) {
     core.setFailed(`Some deployment has failed. See ${inputs.workflowURL} for the summary.`)
     return outputs
   }
+
+  core.summary.addHeading('wait-for-deployment summary', 2)
+  core.summary.addRaw(outputs.summary)
+  await core.summary.write()
   core.info(`You can see the summary at ${inputs.workflowURL}`)
   return outputs
 }
