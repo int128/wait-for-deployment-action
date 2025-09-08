5943be58e323adc4438d819d22debd8d109a4a1f
Hidetake Iwata <int128@gmail.com>
2025-09-08 14:27:45 +0900

    Migrate to @octokit/action

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,19 @@ import * as github from './github.js'
 import { run } from './run.js'
 
 const main = async (): Promise<void> => {
-  const outputs = await run({
-    filterEnvironments: core.getMultilineInput('filter-environments'),
-    excludeEnvironments: core.getMultilineInput('exclude-environments'),
-    until: parseUntil(core.getInput('until', { required: true })),
-    initialDelaySeconds: Number.parseInt(core.getInput('initial-delay-seconds', { required: true })),
-    periodSeconds: Number.parseInt(core.getInput('period-seconds', { required: true })),
-    timeoutSeconds: Number.parseInt(core.getInput('timeout-seconds')) || null,
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    deploymentSha: core.getInput('deployment-sha', { required: true }),
-    token: core.getInput('token', { required: true }),
-    workflowURL: `${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}`,
-  })
+  const outputs = await run(
+    {
+      filterEnvironments: core.getMultilineInput('filter-environments'),
+      excludeEnvironments: core.getMultilineInput('exclude-environments'),
+      until: parseUntil(core.getInput('until', { required: true })),
+      initialDelaySeconds: Number.parseInt(core.getInput('initial-delay-seconds', { required: true })),
+      periodSeconds: Number.parseInt(core.getInput('period-seconds', { required: true })),
+      timeoutSeconds: Number.parseInt(core.getInput('timeout-seconds')) || null,
+      deploymentSha: core.getInput('deployment-sha', { required: true }),
+    },
+    github.getOctokit(),
+    await github.getContext(),
+  )
 
   core.setOutput('progressing', outputs.conclusion.progressing)
   core.setOutput('succeeded', outputs.conclusion.succeeded)
