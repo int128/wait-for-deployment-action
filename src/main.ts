9dc3e936eddec27789667c166008148b229e1e73
Hidetake Iwata <int128@gmail.com>
2026-01-13 16:12:33 +0900

    Fix polling if timeout-seconds is zero (#1093)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import assert from 'node:assert'
 import * as core from '@actions/core'
 import * as github from './github.js'
 import { run } from './run.js'
@@ -8,9 +9,9 @@ const main = async (): Promise<void> => {
       filterEnvironments: core.getMultilineInput('filter-environments'),
       excludeEnvironments: core.getMultilineInput('exclude-environments'),
       until: parseUntil(core.getInput('until', { required: true })),
-      initialDelaySeconds: Number.parseInt(core.getInput('initial-delay-seconds', { required: true }), 10),
-      periodSeconds: Number.parseInt(core.getInput('period-seconds', { required: true }), 10),
-      timeoutSeconds: Number.parseInt(core.getInput('timeout-seconds'), 10) || null,
+      initialDelaySeconds: getRequiredIntInput('initial-delay-seconds'),
+      periodSeconds: getRequiredIntInput('period-seconds'),
+      timeoutSeconds: getOptionalIntInput('timeout-seconds'),
       deploymentSha: core.getInput('deployment-sha', { required: true }),
       summaryMarkdownFlavor: parseSummaryMarkdownFlavor(core.getInput('summary-markdown-flavor', { required: true })),
     },
@@ -34,6 +35,22 @@ const main = async (): Promise<void> => {
   core.startGroup('outputs.json')
   core.info(JSON.stringify(outputs.json, undefined, 2))
   core.endGroup()
+}
+
+const getRequiredIntInput = (name: string): number => {
+  const n = Number.parseInt(core.getInput(name, { required: true }), 10)
+  assert(Number.isSafeInteger(n), `${name} must be an integer`)
+  return n
+}
+
+const getOptionalIntInput = (name: string): number | null => {
+  const v = core.getInput(name)
+  if (v === '') {
+    return null
+  }
+  const n = Number.parseInt(v, 10)
+  assert(Number.isSafeInteger(n), `${name} must be an integer`)
+  return n
 }
 
 const parseUntil = (s: string): 'completed' | 'succeeded' => {
